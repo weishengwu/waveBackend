96d786cd5819a2a44c6f1df2c37ecf89e3cfd0bc
Weisheng Wu <weishenggwuu@gmail.com>
2019-02-22 15:07:30 -0800

    fixed formatting

--- Client.java
+++ Client.java
@@ -1,4 +1,4 @@
- import java.io.*;
+import java.io.*;
 import java.net.*;
 import java.util.*;
 
@@ -11,50 +11,51 @@ public class Client
   private static byte[] buffer;
 
   public static void main(String[] args)
+  {
+    try
     {
-      try
-        {
-           host=InetAddress.getLocalHost();
-        }
-        catch(UnknownHostException uhEx)
-        {
-           System.out.println("HOST ID not found.. ");
-           System.exit(1);
-         }
-            accessServer();
-       }
-     private static void accessServer()
-{
- try 
+      host=InetAddress.getLocalHost();
+    }
+    catch(UnknownHostException uhEx)
+    {
+      System.out.println("HOST ID not found.. ");
+      System.exit(1);
+    }
+    accessServer();
+  }
+
+  private static void accessServer()
+  {
+    try 
     {
       datagramSocket=new DatagramSocket();
       Scanner userEntry=new Scanner(System.in);
       String message="",response="";
-       do
-       {
-           System.out.println("enter message :");
-           message=userEntry.nextLine();
-           if(!message.equals("***CLOSE***"))
-             {
-                outPacket=new DatagramPacket(message.getBytes(),message.length(),host,PORT);
-           datagramSocket.send(outPacket);
-           buffer=new byte[256];
-           inPacket=new DatagramPacket(buffer,buffer.length);
-           datagramSocket.receive(inPacket);
-           response=new String(inPacket.getData(),0,inPacket.getLength());
-            System.out.println(" \n SERVER-->>" +response);
+      do
+      {
+        System.out.println("enter message :");
+        message=userEntry.nextLine();
+        if(!message.equals("***CLOSE***"))
+        {
+          outPacket=new DatagramPacket(message.getBytes(),message.length(),host,PORT);
+          datagramSocket.send(outPacket);
+          buffer=new byte[256];
+          inPacket=new DatagramPacket(buffer,buffer.length);
+          datagramSocket.receive(inPacket);
+          response=new String(inPacket.getData(),0,inPacket.getLength());
+          System.out.println(" \n SERVER-->>" +response);
         }
-    }while(!message.equals("***CLOSE***"));
-}
-catch(IOException ioEx)
-  {
-   ioEx.printStackTrace();
-  }
+      }while(!message.equals("***CLOSE***"));
+    }
+    catch(IOException ioEx)
+    {
+      ioEx.printStackTrace();
+    }
 
-finally
-  {
-     System.out.println("\n closing connection.... ");
+    finally
+    {
+      System.out.println("\n closing connection.... ");
       datagramSocket.close();
-   }
-}
+    }
+  }
 }
--- Server.java
+++ Server.java
@@ -3,35 +3,35 @@ import java.net.*;
 
 public class Server
 {
-   private static final int PORT=1234;
-   private static DatagramSocket datagramSocket;
-   private static DatagramPacket inPacket,outPacket;
-   private static byte[] buffer;
+  private static final int PORT=1234;
+  private static DatagramSocket datagramSocket;
+  private static DatagramPacket inPacket,outPacket;
+  private static byte[] buffer;
 
-   public static void main(String[] args)
-    { 
-      System.out.println("opening port \n");
-       try
-        {
-         datagramSocket=new DatagramSocket(PORT);
-        }
-        catch(SocketException sockEx)
-        {
-            System.out.println("unable to open ");
-            System.exit(1);
-        }
+  public static void main(String[] args)
+  { 
+    System.out.println("opening port \n");
+    try
+    {
+      datagramSocket=new DatagramSocket(PORT);
+    }
+      catch(SocketException sockEx)
+    {
+      System.out.println("unable to open ");
+      System.exit(1);
+    }
       handleClient();
     }
-   private static void handleClient()
-{
-   try
-     {
-       String messageIn,messageOut;
-       int numMessages=0;
-       InetAddress clientAddress=null;
-       int clientPort;
+  private static void handleClient()
+  {
+    try
+    {
+      String messageIn,messageOut;
+      int numMessages=0;
+      InetAddress clientAddress=null;
+      int clientPort;
       do
-       {
+      {
         buffer= new byte[256];
         inPacket=new DatagramPacket(buffer,buffer.length);
         datagramSocket.receive(inPacket);
@@ -41,20 +41,20 @@ public class Server
         System.out.print(clientAddress);
         System.out.print(" : ");
         System.out.println(messageIn);
-numMessages++;
-messageOut="message" + numMessages + ": " + messageIn;
-outPacket=new DatagramPacket(messageOut.getBytes(),messageOut.length(),clientAddress,clientPort);
-datagramSocket.send(outPacket);
-}while(true);
-}
-catch(IOException ioEx)
-  {
-     ioEx.printStackTrace();
+        numMessages++;
+        messageOut="message" + numMessages + ": " + messageIn;
+        outPacket=new DatagramPacket(messageOut.getBytes(),messageOut.length(),clientAddress,clientPort);
+        datagramSocket.send(outPacket);
+      }while(true);
+    }   
+    catch(IOException ioEx)
+    {
+      ioEx.printStackTrace();
+    }
+    finally
+    {
+      System.out.println("\n Closing connection.. ");
+      datagramSocket.close();
+    }
   }
-finally
-  {
-    System.out.println("\n Closing connection.. ");
-datagramSocket.close();
-}
-}
 }
