f8eed9a668552a4c6e7e542098e83b2b730e94c3
Ken Le <kle9597@gmail.com>
2019-02-26 00:53:22 -0800

    Added documentation and made print statements for determining state

--- Server.java
+++ Server.java
@@ -2,50 +2,60 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
+	// Declare variables
 	private static final int PORT = 1234;
 	private static DatagramSocket datagramSocket;
 	private static DatagramPacket inPacket, outPacket;
 	private static byte[] buffer;
 
 	public static void main(String[] args) {
-		System.out.println("opening port \n");
+		System.out.println("Opening Port...");
 		try {
 			datagramSocket = new DatagramSocket(PORT);
+			System.out.println("Datagram socket properly opened at port " + PORT);
+			System.out.println("******************************************************************* \n");
 		} catch (SocketException sockEx) {
-			System.out.println("unable to open ");
+			System.out.println("Unable To Open...");
 			System.exit(1);
 		}
 		handleClient();
 	}
 
+	/** 
+	 * Handles the client request and sends a message back to the client 
+	 */
 	private static void handleClient() {
 		try {
-			String messageIn, messageOut;
-			int numMessages = 0;
+			String messageIn;
+			String messageOut;
 			InetAddress clientAddress = null;
 			int clientPort;
+			int numRequests = 1;
+
 			do {
+				// This section of code for receiving the UDP packets
 				buffer = new byte[256];
 				inPacket = new DatagramPacket(buffer, buffer.length);
 				datagramSocket.receive(inPacket);
 				clientAddress = inPacket.getAddress();
 				clientPort = inPacket.getPort();
 				messageIn = new String(inPacket.getData(), 0, inPacket.getLength());
-				System.out.println("client address is: " + clientAddress);
-				System.out.println("client port is: " + clientPort);
+				System.out.println("Request #" + numRequests);
+				System.out.println("Incoming client request from " + clientAddress + " at port " + clientPort);
+				System.out.println("Message: " + messageIn);
 
-				System.out.print(clientAddress);
-				System.out.print(" : ");
-				System.out.println(messageIn);
-				numMessages++;
-				messageOut = "message" + numMessages + ": " + messageIn;
+				// This section of code for sending the UDP packets
+				messageOut = "message" + numRequests + ": " + messageIn;
 				outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), clientAddress, clientPort);
 				datagramSocket.send(outPacket);
+				numRequests++;
+				System.out.println("Successfully sent response back to client at address " + clientAddress + "\n");
+				System.out.println("******************************************************************* \n");
 			} while (true);
 		} catch (IOException ioEx) {
 			ioEx.printStackTrace();
 		} finally {
-			System.out.println("\n Closing connection.. ");
+			System.out.println("\n Closing connection...");
 			datagramSocket.close();
 		}
 	}
