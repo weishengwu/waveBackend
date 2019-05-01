fa2e93987631ea22693ac3ab4cfd1fcdb108fad3
Ken Le <kle9597@gmail.com>
2019-04-30 23:28:58 -0700

    Added map reduce interface

---
+++ MapReduceInterface.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+import com.google.gson.*;
+
+public interface MapReduceInterface {
+  public void map(String key, JsonObject value, DFS context, String file) throws IOException;
+  public void reduce(String key, JsonObject values, DFS context, String file) throws IOException;
+}
